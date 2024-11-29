d31ccbf3e2ffca4d119c98f549f83129a067129e
metehanpk <metehanhacer2@gmail.com>
2024-11-29 03:23:54 +0300

    Video referansları sıkıştırılmış dosyalarla güncellendi

--- main.js
+++ main.js
@@ -112,70 +112,70 @@ const portfolioItems = [
         title: 'Oyun Editi Örneği',
         description: 'Zula oyun montajı',
         thumbnail: 'assets/zula.jpg',
-        video: 'assets/zula.mp4',
+        video: 'assets/zula_compressed.mp4',
         type: 'game'
     },
     {
         title: 'Tekstil Reklamı',
         description: 'Kurumsal tanıtım',
         thumbnail: 'assets/corporate.jpg',
-        video: 'assets/corporate.mp4',
+        video: 'assets/corporate_compressed.mp4',
         type: 'corporate'
     },
     {
         title: 'Emlak Reklamı',
         description: 'Emlak tanıtım',
         thumbnail: 'assets/yeni-klip.jpg',
-        video: 'assets/yeni-klip.mp4',
+        video: 'assets/yeni_klip_compressed.mp4',
         type: 'estate'
     },
     {
         title: 'Halloween',
         description: 'Cadılar bayramında eğlenceli anlar',
         thumbnail: 'assets/Ekran görüntüsü 2024-11-26 075159.png',
-        video: 'assets/halloween.mp4',
+        video: 'assets/halloween_compressed.mp4',
         type: 'game'
     },
     {
         title: 'İçecek Hazırlama',
         description: 'İçecek hazırlama videosu',
         thumbnail: 'assets/Ekran görüntüsü 2024-11-26 195713.png',
-        video: 'assets/son deneme.mp4',
+        video: 'assets/son_deneme_compressed.mp4',
         type: 'special'
     },
     {
         title: 'Sirk Videosu',
         description: 'Kurumsal tanıtım',
         thumbnail: 'assets/Ekran görüntüsü 2024-11-26 193557.png',
-        video: 'assets/sirk.mp4',
+        video: 'assets/sirk_compressed.mp4',
         type: 'corporate'
     },
     {
         title: 'Oyun Editi Örneği 2',
         description: 'Zula oyun montajı',
         thumbnail: 'assets/Ekran görüntüsü 2024-11-26 063458.png',
-        video: 'assets/4.mp4',
+        video: 'assets/4_compressed.mp4',
         type: 'game'
     },
     {
         title: 'Tekstil Reklamı 2',
         description: 'Kurumsal Tanıtım',
         thumbnail: 'assets/Ekran görüntüsü 2024-11-26 235137.png',
-        video: 'assets/3.mp4',
+        video: 'assets/3_compressed.mp4',
         type: 'special'
     },
     {
         title: 'Sosyal Medya İçin Mizah İçerikleri',
         description: '',
         thumbnail: 'assets/Ekran görüntüsü 2024-11-27 013848.png',
-        video: 'assets/Comp 1.mp4',
+        video: 'assets/comp_1_compressed.mp4',
         type: 'center'
     },
     {
         title: 'Logo Animasyonu Örneği',
         description: 'Animasyon Çalışmalarım',
         thumbnail: 'assets/Ekran görüntüsü 2024-11-28 201449.png',
-        video: 'assets/alevli adidas.mp4',
+        video: 'assets/alevli_adidas_compressed.mp4',
         type: 'center-main'
     }
 ];
